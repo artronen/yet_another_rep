d4fa5e929d0589e9f76089c5dfe6cba209ef2472
a.mironenko <a.mironenko@billingonline.com>
2021-11-03 21:49:13 +0300

    adding requirememts file and first test

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en",
+        help="choose language for testing site",
+    )
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser = None
+
+    options = Options()
+    options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+    print(f"\nstart browser on {user_language} language")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
